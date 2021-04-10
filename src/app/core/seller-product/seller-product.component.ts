3641da2322e8325a9f4e8e934c7e57c3363ef77d
Faisalskp <faisalsaleem000786@gmail.com>
2021-04-10 23:54:57 +0500

    codeUpdated

--- src/app/core/seller-product/seller-product.component.ts
+++ src/app/core/seller-product/seller-product.component.ts
@@ -9,16 +9,38 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./seller-product.component.scss']
 })
 export class SellerProductComponent implements OnInit {
-  ProductForm:any
+  ProductForm: any
   submitted = false;
+  fileToUpload: any;
+  imageUrl: any;
 
-  constructor(private apiService : ApiService,public router: Router) { }
+  url: any
+  format: any
+
+  constructor(private apiService: ApiService, public router: Router) { }
 
   ngOnInit(): void {
     this.ProductForm = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
     })
+  }
+
+
+  onSelectFile(event: any) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      if (file.type.indexOf('image') > -1) {
+        this.format = 'image';
+      }
+      reader.onload = (event) => {
+        this.url = (<FileReader>event.target).result;
+      }
+    }
   }
 
   get f() { return this.ProductForm.controls; }
@@ -28,6 +50,13 @@ export class SellerProductComponent implements OnInit {
     if (this.ProductForm.invalid) {
       return;
     }
+    const finalObject = {
+      name:this.ProductForm.controls.name.value,
+      description:this.ProductForm.controls.description.value,
+      image:this.ProductForm.controls.image.value,
+      price:this.ProductForm.controls.price.value,
+    }
+    console.log(finalObject)
   }
 
 }
